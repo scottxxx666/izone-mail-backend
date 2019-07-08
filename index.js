28df06606459c03e669c1279a45eef21fccb7efa
Scott Kao <scottxxx666@gmail.com>
2019-07-08 18:01:56 +0800

    Extract send notification

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
-const got = require('got');
-const AWS = require("aws-sdk");
-const FormData = require('form-data');
 const user = require('./repositories/user');
 const izone = require('./repositories/izone');
-
-AWS.config.update({
-    region: "ap-northeast-1",
-});
-
-const docClient = new AWS.DynamoDB.DocumentClient();
-
-async function sendNotification(member, blog, token) {
-    console.log(member, blog, token)
-
-    const form = new FormData();
-    form.append('message', member.name + ' 發了封新 mail 唷！\n' + blog.link);
-
-    const url = 'https://notify-api.line.me/api/notify';
-    const headers = {Authorization: 'Bearer ' + token};
-
-    await got(url, {headers: headers, body: form});
-}
+const notification = require('./notification');
 
 async function notifyUsers(blogs, member) {
     const users = await user.all();
     await Promise.all(blogs.map(async blog => {
-        await Promise.all(users.map(user => sendNotification(member, blog, user.access_token)));
+        await Promise.all(users.map(user => notification.send(member, blog, user.access_token)));
     }));
 }
 
---
+++ notification.js
@@ -0,0 +1,18 @@
+const got = require('got');
+const FormData = require('form-data');
+
+const sendNotification = async function (member, blog, token) {
+    console.log(member, blog, token)
+
+    const form = new FormData();
+    form.append('message', member.name + ' 發了封新 mail 唷！\n' + blog.link);
+
+    const url = 'https://notify-api.line.me/api/notify';
+    const headers = {Authorization: 'Bearer ' + token};
+
+    await got(url, {headers: headers, body: form});
+}
+
+module.exports = {
+    send: sendNotification,
+}
