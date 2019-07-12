b89480453a45208becacbe386733bf9776c62d80
Scott Kao <scottxxx666@gmail.com>
2019-07-12 19:31:58 +0800

    Fix for airbnb eslint

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const user = require('./repositories/user');
-const izone = require('./repositories/izone');
+const userRepo = require('./repositories/user');
+const izoneRepo = require('./repositories/izone');
 const notification = require('./notification');
 
 async function notifyUsers(blogs, member) {
-    const users = await user.all();
-    await Promise.all(blogs.map(async blog => {
-        await Promise.all(users.map(user => notification.send(member, blog, user.access_token)));
-    }));
+  const users = await userRepo.all();
+  await Promise.all(blogs.map(async (blog) => {
+    await Promise.all(users.map(user => notification.send(member, blog, user.access_token)));
+  }));
 }
 
 async function updateLastId(blogs, member) {
-    const lastId = Math.max(...blogs.map(blog => blog.id));
-    await izone.updateLastId(member, lastId);
+  const lastId = Math.max(...blogs.map(blog => blog.id));
+  await izoneRepo.updateLastId(member, lastId);
 }
 
 exports.handler = async function () {
-    const members = await izone.members();
-    await Promise.all(members.map(async member => {
-        const blogs = await izone.getNewBlogs(member);
-        await notifyUsers(blogs, member);
-        await updateLastId(blogs, member);
-    }));
-    return 'Success';
+  const members = await izoneRepo.members();
+  await Promise.all(members.map(async (member) => {
+    const blogs = await izoneRepo.getNewBlogs(member);
+    await notifyUsers(blogs, member);
+    await updateLastId(blogs, member);
+  }));
+  return 'Success';
 };
